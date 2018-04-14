531cfaa18b9f78ee4aae6a1787035eab0054eb42
neeral <neeral@users.noreply.github.com>
2018-04-13 20:27:26 -0700

    Add build script
    Summary:
    An alternative to setting up a VM with Cockroach dev dependencies, we
    should be able to build everything in Docker. This script builds
    auxiliary binaries: roachprod, roachtest and workload. These are
    required for testing. They are built using the Golang standard
    `go install` and output to $GOPATH/bin.
    
    Test Plan:
    Run `build.sh`. Verify return code is 0. Verify that roachprod, workload
    and roachtest binaries are newly created under $GOPATH/bin
    
    Reviewers: garvit, kaavee, Alfred
    
    Reviewed By: kaavee
    
    Tags: #cly
    
    Differential Revision: https://phabricator.rubrik.com/D44615

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "GOPATH=$GOPATH"
+COCKROACH_BUILDER=$GOPATH/src/github.com/cockroachdb/cockroach/build/builder.sh
+if [ -f $COCKROACH_BUILDER ]; then
+  image=$(grep 'image=' $COCKROACH_BUILDER | cut -f 2 -d '=')
+  version=$(grep 'version=' $COCKROACH_BUILDER | cut -f 2 -d '=')
+    echo "Using Docker $image:$version"
+else
+  echo "CockroachDB repo not found."
+  exit 1
+fi
+
+DOCKER_RUN="docker run -i -u $(id -u):$(id -g) --rm \
+  --volume=$GOPATH/bin:/go/bin \
+  --volume=$GOPATH/src/github.com/cockroachdb:/go/src/github.com/cockroachdb \
+  $image:$version"
+
+echo "Building roachprod"
+$DOCKER_RUN go install github.com/cockroachdb/roachprod
+
+echo "Building roachtest"
+$DOCKER_RUN go install github.com/cockroachdb/cockroach/pkg/cmd/roachtest
+
+echo "Building workload"
+$DOCKER_RUN go install github.com/cockroachdb/cockroach/pkg/cmd/workload
+echo "Done"
